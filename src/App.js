e4569def0457711ebd13d689b5d5dcb7bf23c404
Deepak Aggarwal <deepak.aggarwal9@gmail.com>
2020-04-15 17:32:19 +0530

    Basic ant design theme integrated and dashboard layou

--- src/App.js
+++ src/App.js
@@ -1,95 +1,16 @@
-import React, {Fragment} from "react";
-import { Container, Typography, Select, MenuItem } from "@material-ui/core";
-import { makeStyles } from "@material-ui/styles";
-import MapContainer from "./features/map/MapContainer";
-import Dashboard from "./features/dashboard/Dashboard";
-import Patient from "./features/patient/Patient";
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import Box from "@material-ui/core/Box";
+import React from "react";
 
-const containerStyle = {
-  maxWidth: "100%",
-  padding: 0,
-};
-
-const useStyles = makeStyles({
-  withBar: {
-    ...containerStyle,
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-  },
-  logo: {
-    height: 50,
-    width: 50,
-  },
-  bar: {
-    ...containerStyle,
-    backgroundColor: "#d7d7d7",
-    display: "inline-flex",
-    lineHeight: 20,
-    alignItems: "center",
-    padding: 5,
-  },
-  app: {
-    ...containerStyle,
-    flexGrow: 1,
-    width: "100%",
-    display: "flex",
-    // flexDirection: "column",
-  },
-  patient: {
-    ...containerStyle,
-    width: "450px",
-    flexGrow: 0,
-    backgroundColor: "#f2f2f2"
-  },
-  main: {
-    ...containerStyle,
-    flexGrow: 1,
-    display: "flex",
-
-    flexDirection: "column",
-  },
-  dashboard: {
-    ...containerStyle,
-  },
-  mapContainer: {
-    ...containerStyle,
-    flexGrow: 6,
-  },
-});
-
+import "./App.css";
+import { Button, DatePicker, Table } from "antd";
+import "./App.css";
+import { dataSource, columns } from "./dataSource";
+import { LayoutComponent } from "./components/shared";
 function App() {
-  const classes = useStyles();
-
   return (
-    <Container className={classes.withBar}>
-      <Container className={classes.bar}>
-        <img className={classes.logo} src="mainlogo.png"/>
-        <Typography style={{color: "#3e3e3e", fontWeight: "bold", textTransform: "uppercase", padding: "5px", fontSize: "1.2rem", width:"300px"}}>Data4Life</Typography>
-        <Typography style={{color: "#3e3e3e", fontWeight: "bold", textTransform: "uppercase", padding: "5px", fontSize: "1.1rem"}}>Stockholm Region</Typography>
-
-        <Box style={{padding: "5px", marginLeft: "auto", display: "flex", alignItems: "center"}}>
-          <Typography style={{padding: "5px", fontStyle: "italic", marginLeft: "auto"}}>Logged in as Lotta Lundin</Typography>
-          <AccountCircleIcon style={{ fontSize: 40 }} />
-        </Box>
-
-      </Container>
-      <Container className={classes.app}>
-        <Container className={classes.patient}>
-          <Patient />
-        </Container>
-        <Container className={classes.main}>
-          <Container className={classes.dashboard}>
-            <Dashboard />
-          </Container>
-          <Container className={classes.mapContainer}>
-            <MapContainer  style={{position: "relative"}} />
-          </Container>
-        </Container>
-      </Container>
-    </Container>
+    <LayoutComponent>
+      <DatePicker></DatePicker> <Button type="primary">Button</Button>
+      <Table dataSource={dataSource} columns={columns} />;
+    </LayoutComponent>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
