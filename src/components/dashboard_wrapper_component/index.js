306977fb47464bf11fb30654f1aeefb5c18352b4
Deepak Aggarwal <deepak.aggarwal9@gmail.com>
2020-09-05 03:20:36 +0530

    Initial commit

--- src/components/dashboard_wrapper_component/index.js
+++ src/components/dashboard_wrapper_component/index.js
@@ -4,20 +4,29 @@ import { PlusOutlined } from "@ant-design/icons";
 import Dashboard from "../dashboard/Dashboard";
 import MapContainer from "../map/MapContainer";
 import Patient from "../patient/Patient";
+import IssueCertificate from "../issue_certificate/IssueCertificate";
 import AdminLayoutComponent from "../shared/admin-layout";
 import "./main-style.css";
 
 export default class DashboradWrapperComponent extends React.Component {
-    state = { visible: false };
-    showDrawer = () => {
+    state = { showAddPatientVisible: false, showIssueCertificateVisible: false };
+
+    showAddPatientDrawer = () => {
         this.setState({
-            visible: true,
+            showAddPatientVisible: true,
+        });
+    };
+
+    showIssueCertificateDrawer = () => {
+        this.setState({
+            showIssueCertificateVisible: true,
         });
     };
 
     onClose = () => {
         this.setState({
-            visible: false,
+            showAddPatientVisible: false,
+            showIssueCertificateVisible: false
         });
     };
 
@@ -28,15 +37,30 @@ export default class DashboradWrapperComponent extends React.Component {
                     title="Patient"
                     className="custom-drawer"
                     onClose={this.onClose}
-                    visible={this.state.visible}
+                    visible={this.state.showAddPatientVisible}
                     bodyStyle={{ paddingBottom: 80 }}
                     destroyOnClose={true}
                 >
                     <Patient />
                 </Drawer>
 
-                <Button type="primary" onClick={this.showDrawer}>
+                <Drawer
+                    title="Issue Certificate"
+                    className="custom-drawer"
+                    onClose={this.onClose}
+                    visible={this.state.showIssueCertificateVisible}
+                    bodyStyle={{ paddingBottom: 80 }}
+                    destroyOnClose={true}
+                >
+                    <IssueCertificate />
+                </Drawer>
+
+                <Button type="primary dashboard-buttons" onClick={this.showAddPatientDrawer}>
                     <PlusOutlined /> Patient
+                </Button>
+
+                <Button type="primary issue-certificate-button dashboard-buttons" onClick={this.showIssueCertificateDrawer}>
+                    <PlusOutlined /> Issue Certificate
                 </Button>
 
                 <div className="dashboard">
